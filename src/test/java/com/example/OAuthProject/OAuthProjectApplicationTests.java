d34a5994b9133db6459c0518306d075150e8e90e
Ida Kamali <idakamali@Idas-Air.lan>
2021-03-17 23:52:16 -0500

    first commit

---
+++ src/test/java/com/example/OAuthProject/OAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
